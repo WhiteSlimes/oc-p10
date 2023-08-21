38b6a6ad466cbcc4ea6be12dd84efd5fe9de5522
WhiteSlimes <43409869+WhiteSlimes@users.noreply.github.com>
2023-08-21 16:43:52 +0200

    ~ Versioning 1.0 ~
    - Change Slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -11,10 +11,9 @@ const Slider = () => {
     new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
-      5000
-    );
+    if (byDateDesc && byDateDesc.length > 0) {
+      setTimeout(() => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0), 5000);
+    };
   };
   useEffect(() => {
     nextCard();
@@ -23,12 +22,7 @@ const Slider = () => {
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
         <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
+          <div key={event.title} className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}>
             <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
@@ -39,17 +33,18 @@ const Slider = () => {
             </div>
           </div>
           <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
+          <div className="SlideCard__pagination">
+            {byDateDesc.map((_, radioIdx) => (
+              <input
+                key={`${radioIdx+1}`}
+                type="radio"
+                name="radio-button"
+                checked={index === radioIdx}
+                onChange={() => setIndex(radioIdx)}
+              />
+            ))}
           </div>
+        </div>
         </>
       ))}
     </div>
