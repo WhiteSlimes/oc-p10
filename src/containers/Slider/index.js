41e5acd13b623133a8979663ef45fe1e87cd5f96
WhiteSlimes <43409869+WhiteSlimes@users.noreply.github.com>
2023-09-06 10:10:11 +0200

    ~ Versioning 1.2 ~
    - Change key

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -51,7 +51,7 @@ const Slider = () => {
           <div className="SlideCard__pagination">
             {byDateDesc.map((_, radioIdx) => (
               <input
-                key={`${event.id}`}
+                key={`${byDateDesc[radioIdx].title}`}
                 type="radio"
                 name="radio-button"
                 checked={index === radioIdx}
